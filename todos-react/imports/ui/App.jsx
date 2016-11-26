f4b007e71ba5898035ee2ca1e6b6f5beefebb2a2
bigboss <825169156@qq.com>
2016-11-26 17:01:23 +0800

    second commit

--- todos-react/imports/ui/App.jsx
+++ todos-react/imports/ui/App.jsx
@@ -1,10 +1,21 @@
 import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
 import { createContainer } from 'meteor/react-meteor-data'
 
 import { Tasks } from '../api/tasks.js';
 import Task from './Task.jsx';
 
 class App extends Component {
+  handleSubmit(event) {
+    event.preventDefault();
+    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+    Tasks.insert({
+      text,
+      createdAt: new Date(),
+    });
+
+    ReactDOM.findDOMNode(this.refs.textInput).value = '';
+  }
   renderTasks() {
     return this.props.tasks.map((task) => (
       <Task key={task._id} task={task} />
@@ -15,8 +26,15 @@ class App extends Component {
       <div className="container">
         <header>
           <h1>Todo List</h1>
-        </header>
 
+        <form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
+          <input
+            type="text"
+            ref="textInput"
+            placeholder="Type to add new tasks"
+            />
+        </form>
+      </header>
         <ul>
           {this.renderTasks()}
         </ul>
@@ -31,7 +49,7 @@ App.propTypes = {
 
 export default createContainer(() => {
   return {
-    tasks: Tasks.find({}).fetch(),
+    tasks: Tasks.find({}, { sort: {createdAt: -1 } }).fetch(),
   };
 }, App);
 
